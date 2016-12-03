e307545066ef50ccaa12b64da23e1797f085097d
Julie MacDonell <julie.macdonell@stylight.com>
2016-12-03 11:25:55 +0100

    sentences

--- SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/Sentences.java
+++ SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/Sentences.java
@@ -14,10 +14,52 @@ public class Sentences {
     public List<String> returnWords(){
 
         availableSentences.add("Hello beautiful");
-        availableSentences.add("Hello beautiful");
-        availableSentences.add("Hello beautiful");
-        availableSentences.add("Hello beautiful");
-
+        availableSentences.add("I can’t control myself when I’m near you.");
+        availableSentences.add("I can’t get enough of you.");
+        availableSentences.add("I can’t get last night out of my mind.");
+        availableSentences.add("I had such a good dream about you last night.");
+        availableSentences.add("I just got out of the shower.");
+        availableSentences.add("I like thinking about you touching yourself.");
+        availableSentences.add("I love looking into your eyes.");
+        availableSentences.add("I love the way you make me feel.");
+        availableSentences.add("I love thinking about you spanking me and pulling my hair.");
+        availableSentences.add("I love when you talk like that.");
+        availableSentences.add("I miss feeling you inside of me.");
+        availableSentences.add("I need you inside of me right now.");
+        availableSentences.add("I need you inside of me right now.");
+        availableSentences.add("I read this article about anal today, and it got me thinking.");
+        availableSentences.add("I thought of something I want to do to you tonight.");
+        availableSentences.add("I want you to do exactly what you did last time.");
+        availableSentences.add("I want you to kiss me.");
+        availableSentences.add("I want you to lie back and let me take care of you tonight.");
+        availableSentences.add("I want you to touch me.");
+        availableSentences.add("I want you.");
+        availableSentences.add("I want you.");
+        availableSentences.add("It’s so hot to imagine you tying me up.");
+        availableSentences.add("It’s too bad you’re not here right now.");
+        availableSentences.add("I’m going to let you do anything you want to me.");
+        availableSentences.add("I’m going to make you beg for it.");
+        availableSentences.add("I’m going to make you come so hard.");
+        availableSentences.add("I’m going to ravage you the second you get in the door.");
+        availableSentences.add("I’m not going to let you come until you beg me for permission.");
+        availableSentences.add("I’m picturing you and me with another woman.");
+        availableSentences.add("I’m so wet right now.");
+        availableSentences.add("I’m too busy tonight. You can’t have me until tomorrow.");
+        availableSentences.add("I’m touching myself right now thinking about you.");
+        availableSentences.add("I’ve been a bad girl and I need someone to punish me.");
+        availableSentences.add("I’ve got a surprise waiting for you.");
+        availableSentences.add("I’ve never felt as attracted to anyone else as I am to you.");
+        availableSentences.add("My clothes are coming off the second you get home.");
+        availableSentences.add("Tell me more.");
+        availableSentences.add("Tell me what you think about when you masturbate.");
+        availableSentences.add("Tell me what you think about when you masturbate.");
+        availableSentences.add("That turns me on so much.");
+        availableSentences.add("The anticipation is killing me.");
+        availableSentences.add("What are you wearing right now?");
+        availableSentences.add("What else will you do to me?");
+        availableSentences.add("You always feel so good inside of me.");
+        availableSentences.add("You make me so wet.");
+        availableSentences.add("Your messages have me so distracted.");
 
         return availableSentences;
     }
