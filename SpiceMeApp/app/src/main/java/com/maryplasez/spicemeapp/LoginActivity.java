1aa72e42993cb1aaf2fbdbb0dc4651f94f7b07bc
MariaPlatero <maria.platero.sanchez@stylight.com>
2016-12-03 12:25:02 +0100

    WIP

--- SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/LoginActivity.java
+++ SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/LoginActivity.java
@@ -194,7 +194,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
