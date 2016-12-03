a5902f80c725df3107748366e8d8d177ae5c7302
sebastianseitz <sebastian.seitz@stylight.com>
2016-12-03 16:31:13 +0100

    bakc button

--- SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/SpiceSelectionActivity.java
+++ SpiceMeApp/app/src/main/java/com/maryplasez/spicemeapp/SpiceSelectionActivity.java
@@ -119,44 +119,10 @@ public class SpiceSelectionActivity extends AppCompatActivity {
         vw.setSelected(true);
         Intent intent = new Intent(context, ChatActivity.class);
         intent.putExtra("PUSH", vw.getText());
+        intent.putExtra("USER", this.activeUser);
         startActivity(intent);
         finish();
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-//    public static class PlaceholderFragment extends Fragment {
-//        /**
-//         * The fragment argument representing the section number for this
-//         * fragment.
-//         */
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        /**
-//         * Returns a new instance of this fragment for the given section
-//         * number.
-//         */
-//        public static PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//            Bundle args = new Bundle();
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_spice_selection, container, false);
-//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-//            return rootView;
-//        }
-//    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
